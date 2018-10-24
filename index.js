3bbb5e45d10ad83d25c620a2ef4ccd6161dd7789
Kyle Sit <ktsit@ucsd.edu>
2018-10-24 14:58:25 -0700

    added ESLint tool with precommit enforcement

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 /**
  * index.js
  */
- 
+
 const logSomething = options => ({
   ...options,
-  anotherOption: 'Hello!'
+  anotherOption: 'Hello!',
 });
 const options = logSomething({ one: '1', two: '2' });
 console.log(options);
